b1788918cfce3d394b988784f52161c2dfd147f5
Philipp von Seydlitz <pvseydlitz@me.com>
2019-11-29 10:10:35 +0100

    Add router for first two pages

---
+++ src/Create.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import Grid from './Grid'
+
+export default function Create() {
+  return (
+    <Grid>
+      <Header></Header>
+      <div></div>
+      <Footer></Footer>
+    </Grid>
+  )
+}
---
+++ src/Home.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import styled from 'styled-components/macro'
+import Globalstyles from './Globalstyles'
+import Grid from './Grid'
+import Header from './Header'
+import Message from './Message'
+import MessagesData from './messages'
+import Footer from './Footer'
+
+function Home() {
+  const [messages, setMessages] = useState(MessagesData)
+  const MessageWrapper = styled.div`
+    overflow-y: scroll;
+  `
+  function toggleBookmarked(index) {
+    const message = messages[index]
+    setMessages([
+      ...messages.slice(0, index),
+      { ...message, isBookmarked: !message.isBookmarked },
+      ...messages.slice(index + 1),
+    ])
+    console.log(messages)
+  }
+  const [isOnlyBookmarkShown, setIsOnlyBookmarkShown] = useState(false)
+  return (
+    <Grid>
+      <Globalstyles></Globalstyles>
+      <Header
+        filterMessages={() => setIsOnlyBookmarkShown(!isOnlyBookmarkShown)}
+        filterActive={isOnlyBookmarkShown}
+      ></Header>
+      <MessageWrapper>
+        {isOnlyBookmarkShown
+          ? messages
+              .filter(message => message.isBookmarked === true)
+              .map((message, index) => (
+                <Message
+                  message={message}
+                  key={index}
+                  toggleBookmarked={() => toggleBookmarked(index)}
+                ></Message>
+              ))
+          : messages.map((message, index) => (
+              <Message
+                message={message}
+                key={index}
+                toggleBookmarked={() => toggleBookmarked(index)}
+              ></Message>
+            ))}
+      </MessageWrapper>
+      <Footer></Footer>
+    </Grid>
+  )
+}
+
+export default Home
