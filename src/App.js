c159ab70c51dc40b369b7dad4042525cb1c405af
Philipp von Seydlitz <pvseydlitz@me.com>
2019-12-02 16:52:11 +0100

    Server is accessible from postman

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './Home'
 import Create from './second-page/Create'
+import { getMessages } from './services'
 export default function App() {
+  console.log(getMessages())
   return (
     <Router>
       <Switch>
