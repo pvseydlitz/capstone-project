f0517c155820828592a4ddf04c3716d77e80eff0
Philipp von Seydlitz <pvseydlitz@me.com>
2019-12-11 12:59:17 +0100

    First step

---
+++ server/models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const userSchema = {
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+}
+
+module.exports = mongoose.model('User', userSchema)
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const Message = require('./models/Message')
 const MessageTuev = require('./models/MessageTuev')
+const User = require('./models/User')
 const express = require('express')
 
 mongoose.connect('mongodb://localhost:27017/capstone-project', {
@@ -65,3 +66,61 @@ app.delete('/messagestuev/:id', (req, res) => {
     .then(message => res.json(message))
     .catch(err => res.json(err))
 })
+
+app.get('/', (req, res) => {
+  User.find()
+    .then(user => res.json(user))
+    .catch(err => res.json(err))
+})
+
+//POST route for updating data
+/* app.post('/', function(req, res, next) {
+  // confirm that user typed same password twice
+  if (req.body.password !== req.body.passwordConf) {
+    var err = new Error('Passwords do not match.')
+    err.status = 400
+    res.send('passwords dont match')
+    return next(err)
+  }
+
+  if (
+    req.body.email &&
+    req.body.username &&
+    req.body.password &&
+    req.body.passwordConf
+  ) {
+    var userData = {
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password,
+    }
+
+    User.create(userData, function(error, user) {
+      if (error) {
+        return next(error)
+      } else {
+        req.session.userId = user._id
+        return res.redirect('/profile')
+      }
+    })
+  } else if (req.body.logemail && req.body.logpassword) {
+    User.authenticate(req.body.logemail, req.body.logpassword, function(
+      error,
+      user
+    ) {
+      if (error || !user) {
+        var err = new Error('Wrong email or password.')
+        err.status = 401
+        return next(err)
+      } else {
+        req.session.userId = user._id
+        return res.redirect('/profile')
+      }
+    })
+  } else {
+    var err2 = new Error('All fields required.')
+    err.status = 400
+    return next(err2)
+  }
+})
+ */
